e5cecfd84db68f22033c1be95516212fa4590428
Seba G <sebag.git@gmail.com>
2023-04-15 17:02:09 +0200

    add build for css-language-server

--- build.sh
+++ build.sh
@@ -15,6 +15,11 @@ then
 
 	npm install -g @babel/core@7.21.4 @babel/cli@7.21.0
 	npx babel $TMP_CSS_DIR --out-dir $TMP_CSS_LIB_DIR
+elif [ $1 = "css-lsp" ]
+then
+	echo "css language server"
+	cp -r $TMP_CSS_LIB_DIR/* css-language-server
+	cd css-language-server
+	npm install
+	npm pack
 fi
-
-
