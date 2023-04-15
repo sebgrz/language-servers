53cbaa136506e368ae5c122005826c40b516822f
Seba G <sebag.git@gmail.com>
2023-04-15 16:36:49 +0200

    VSCode build
    - add vscode submodule
    - add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TMP_CSS_DIR=/tmp/css
+TMP_CSS_LIB_DIR=$TMP_CSS_DIR"-lib"
+
+if [ $1 = "vscode" ]
+then
+	echo "vscode build"
+	cd vscode
+	yarn
+	yarn compile
+
+	mkdir $TMP_CSS_DIR
+	cp -r extensions/css-language-features/server/out/* $TMP_CSS_DIR
+
+	npm install -g @babel/core@7.21.4 @babel/cli@7.21.0
+	npx babel $TMP_CSS_DIR --out-dir $TMP_CSS_LIB_DIR
+fi
+
+
